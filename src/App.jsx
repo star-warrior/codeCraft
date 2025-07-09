5c0dccb63bbea5638a8ac70f854c277c3cf4817a
Jay Mehta <69752445+star-warrior@users.noreply.github.com>
2025-07-09 15:03:01 +0530

    Theme Switch Enables

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from "react";
 import "./App.css";
 import Editor from "./pages/Editor";
+import "./pages/Editor.css"; // Import the CSS file
 
 function App() {
   // const [count, setCount] = useState(0);
